608bbec2ed1dcf7ea84dc7cda04ddf7702a8a6fe
Ryan <Ryan@gmail>
2025-03-03 10:22:42 -0500

    more variables more brainstorming

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,4 @@
-import java.lang.classfile.constantpool.ClassEntry;
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,13 +13,23 @@ public class Rectangle {
     private int botRightY;
     private int width;
     private int height;
+    private int[] xRange;
+    private int[] yRange;
 
     public Rectangle(int topLeftX, int topLeftY, int width, int height) {
         this.width = width;
         this.height = height;
         this.topLeftX = topLeftX;
         this.topLeftY = topLeftY;
+        topRightX = topLeftX + width;
+        topRightY = topLeftY;
+        botLeftX = topLeftX;
+        botLeftY = topLeftY - height;
+        botRightX = topLeftX + width;
+        botRightY = topLeftY - width;
 
+        xRange = (topLeftX, topRightX);
+        yRange = (botLeftY, topLeftY);
 
     }
 
@@ -43,7 +53,23 @@ public class Rectangle {
     }
 
     public boolean checkOverlap(Rectangle otherRect) {
-
         return true;
+    }
+
+    public String toString() {
+        return "Rectangle{" +
+                "topLeftX=" + topLeftX +
+                ", topLeftY=" + topLeftY +
+                ", topRightX=" + topRightX +
+                ", topRightY=" + topRightY +
+                ", botLeftX=" + botLeftX +
+                ", botLeftY=" + botLeftY +
+                ", botRightX=" + botRightX +
+                ", botRightY=" + botRightY +
+                ", width=" + width +
+                ", height=" + height +
+                ", xRange=" + Arrays.toString(xRange) +
+                ", yRange=" + Arrays.toString(yRange) +
+                '}';
     }
 }
